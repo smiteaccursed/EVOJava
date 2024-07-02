b815ee514fe74973d45dcda0c11231eb9bb818e6
_smite_ <bulat.2003.05@mail.ru>
2024-07-02 18:34:13 +0300

    task 12

--- Основы Spring boot/12/untitled/src/Main.java
+++ Основы Spring boot/12/untitled/src/Main.java
@@ -1,15 +1,73 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        double a ;
+        double b ;
+        Scanner in= new Scanner(System.in);
+        System.out.println("Введите a");
+        a=in.nextInt();
+        in.nextLine();
+        System.out.println("Введите b");
+        b=in.nextInt();
+        in.nextLine();
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        Calculator calculator = new Calculator(new Adder());
+        System.out.print("Результат a+b: ");
+        calculator.calc(a, b);
+
+        calculator = new Calculator(new Subtractor());
+        System.out.print("Результат a-b: ");
+        calculator.calc(a, b);
+
+        calculator = new Calculator(new Divider());
+        System.out.print("Результат a/b: ");
+        calculator.calc(a, b);
+        calculator = new Calculator(new Multiplier());
+        System.out.print("Результат a*b: ");
+        calculator.calc(a, b);
+
+    }
+}
+
+class Adder implements Operation {
+    @Override
+    public double getResult(double a, double b) {
+        return a + b;
+    }
+}
+
+class Subtractor implements Operation {
+    @Override
+    public double getResult(double a, double b) {
+        return a - b;
+    }
+}
+class Divider implements Operation{
+    @Override
+    public double getResult(double a, double b)
+    {
+        return a/b;
+    }
+}
+class Multiplier implements Operation{
+    @Override
+    public double getResult(double a, double b)
+    {
+        return a*b;
+    }
+}
+
+interface Operation {
+    double getResult(double a, double b);
+}
+
+class Calculator {
+    Operation operation;
+    public Calculator(Operation operation) {
+        this.operation = operation;
+    }
+    public void calc(double a, double b) {
+        System.out.println(operation.getResult(a, b));
     }
 }
